1e69b17c1098a8b14b092a9afaad404d823f85ed
yandong.yan <yandong.yan@upai.com>
2017-05-22 16:04:55 +0800

    add timer calculate thread

--- monitor_janus/monitor_janus.py
+++ monitor_janus/monitor_janus.py
@@ -10,8 +10,8 @@ global threadNameList
 threadNameList = []
 global proccess_name
 proccess_name = ""
-global timing_log
-timing_log = 120
+global timing_log_interval
+timing_log_interval = 120
 global log_path
 log_path = ""
 global result_data
@@ -19,12 +19,16 @@ result_data = {}
 global fp
 fp = None
 
+global lastDataTime
+lastDataTime = 0
+
 
 def get_command_param(configfile='monitor.conf'):
     global proccess_name
     global threadNameList
     global timing_log
     global log_path
+    global timing_log_interval
     with open(configfile,'r') as cfgfile:
         cf = ConfigParser.ConfigParser()
         cf.readfp(cfgfile)
@@ -33,8 +37,7 @@ def get_command_param(configfile='monitor.conf'):
 
         name = cf.get("info","name")
         proccess_name = cf.get("info",'target_proccess_name')
-        gen_log = cf.get("info","generate_unnormal_log")
-        print "streamnumber %s,gen_log %s" % (proccess_name,gen_log)
+        print "proccess_name %s" % (proccess_name)
 
         timing_log = cf.get("info","timing_log_interval")
         thread_name = cf.get("info","target_thread_name")
@@ -54,6 +57,8 @@ def get_command_param(configfile='monitor.conf'):
         #     os.mkdir(log_path)
         # for th in thread_name:
         #     print th
+        timing_log_interval = int(timing_log)
+        print "timing_log_interval=%d" % timing_log_interval
 
 def monitor_thread(proccessName,threadName):
     cmdline = "".join(('pstree -p $(pidof ',proccessName,') |grep ',threadName,'|wc -l'))
@@ -76,7 +81,7 @@ def log_result():
     tmp["time"]=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) 
     log_text = json.dumps(tmp)
     print log_text
-    print type(log_text)
+    #print type(log_text)
     fp.write(log_text+'\n')
     fp.flush() 
 
@@ -86,16 +91,23 @@ def monitor():
     global timing_log
     global log_path
     global fp
+    global timing_log_interval
+    lastDataTime = 0
     fp = open(log_path,"a")
-
-    print "begin"
-    if proccess_name == "":
-        print "proccess name error: ",proccess_name
-        return
-    for th in threadNameList:
-        monitor_thread(proccess_name,th)
-    log_result()
-
+    while True:
+        nowtm = time.time()
+        #print "now time %d",nowtm
+        #nowtm = time.mktime(time.localtime())  
+        diff = int(nowtm - lastDataTime) 
+        if diff >  timing_log_interval:
+            lastDataTime = nowtm
+            if proccess_name == "":
+                print "proccess name error: ",proccess_name
+                continue
+            for th in threadNameList:
+                monitor_thread(proccess_name,th)
+            log_result()
+        time.sleep(1)
     fp.close()
 
 if __name__ == "__main__":
